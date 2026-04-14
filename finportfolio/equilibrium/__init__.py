c74bb71c14f87d9b288bbed22737ce7e081c5742
Miguel Herrera <maherrerav10@gmail.com>
2026-04-14 00:00:43 -0600

    Fix __all__ in equilibrium __init__.py, bump version to 0.1.6

--- finportfolio/equilibrium/__init__.py
+++ finportfolio/equilibrium/__init__.py
@@ -1,4 +1,4 @@
-from .capm import (estimate_beta, capm_expected_return, security_market_line,
+from .capm import (estimate_beta, capm_expected_return, security_market_line, plot_security_market_line,
                    gordon_model, gordon_model_implied_return, apt_expected_return)
-__all__ = ["estimate_beta", "capm_expected_return", "security_market_line", "plot_security_market_line"
+__all__ = ["estimate_beta", "capm_expected_return", "security_market_line", "plot_security_market_line",
            "gordon_model", "gordon_model_implied_return", "apt_expected_return"]
